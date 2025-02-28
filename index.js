64b25fbac09962de9059a05223957f3148e065c4
Sinane <sinane.si-mohamed.externe@emeria.eu>
2025-02-28 23:10:14 +0100

    windows modif

--- index.js
+++ index.js
@@ -6,33 +6,35 @@ const ytdl = require("ytdl-core");
 
 const destDirName = "Music Pack BFC";
 
-const options = { sheetName: "NOM_DE_LA_SHEET" }; // Remplacer par le nom de la feuille
+const options = {  }; // Remplacer par le nom de la feuille
 
 const parser = new PublicGoogleSheetsParser(spreadsheetId, options);
 
 async function parse() {
-  const sheet = await parser.parse();
+  try {
+    const sheet = await parser.parse();
+    console.log(`Number of rows: ${sheet.length}`);
 
-  console.log(sheet.length);
-  for (const row of sheet) {
-    if (row.Lien) {
-      const url = row.Lien;
-      const fileName = `${row.Nom}.mp4`;
+    for (const row of sheet) {
+      if (row.Lien) {
+        const url = row.Lien;
+        const fileName = `${row.Nom}.mp4`;
 
-      const destDir = path.join(__dirname, destDirName);
+        const destDir = path.join(__dirname, destDirName);
 
-      if (!fs.existsSync(destDir)) {
-        fs.mkdirSync(destDir);
+        if (!fs.existsSync(destDir)) {
+          fs.mkdirSync(destDir, { recursive: true });
+        }
+        const dest = path.join(destDir, fileName);
+
+        ytdl(url, { filter: format => format.container === 'mp4' })
+          .pipe(fs.createWriteStream(dest))
+          .on("finish", () => console.log(`Downloaded ${fileName}`))
+          .on("error", (err) => console.error(`Error downloading ${fileName}: ${err}`));
       }
-      const dest = path.join(destDir, fileName);
-
-      ytdl(url)
-        .pipe(fs.createWriteStream(dest))
-        .on("finish", () => console.log(`Downloaded ${fileName}`))
-        .on("error", (err) =>
-          console.error(`Error downloading ${fileName}: ${err}`)
-        );
     }
+  } catch (error) {
+    console.error("An error occurred during parsing or downloading:", error);
   }
 }
 
